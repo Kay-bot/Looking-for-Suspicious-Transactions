09105d5fb0f4ac34510a9d2b97fa5272b3073fa0
Kay Levin <ks.kwanruan@gmail.com>
2023-10-22 17:20:01 +1100

    completed

--- schema.sql
+++ schema.sql
@@ -1,40 +1,40 @@
 -- Create tables
-CREATE TABLE merchant_categories (
+CREATE TABLE merchant_category (
     id INT PRIMARY KEY,
     name VARCHAR(255)
 );
 
-CREATE TABLE merchants (
+CREATE TABLE merchant (
     id INT PRIMARY KEY,
     name VARCHAR(255),
     id_merchant_category INT,
-    FOREIGN KEY (id_merchant_category) REFERENCES merchant_categories(id)
+    FOREIGN KEY (id_merchant_category) REFERENCES merchant_category(id)
 );
 
-CREATE TABLE card_holders (
+CREATE TABLE card_holder (
     id INT PRIMARY KEY,
     name VARCHAR(255)
 );
 
-CREATE TABLE credit_cards (
+CREATE TABLE credit_card (
     card VARCHAR(20) PRIMARY KEY,
     card_holder_id INT,
-    FOREIGN KEY (card_holder_id) REFERENCES card_holders(id)
+    FOREIGN KEY (card_holder_id) REFERENCES card_holder(id)
 );
 
-CREATE TABLE transactions (
+CREATE TABLE transaction (
     id INT PRIMARY KEY,
     date TIMESTAMP,
     amount DECIMAL(10, 2),
     card VARCHAR(20),
     id_merchant INT,
-    FOREIGN KEY (card) REFERENCES credit_cards(card),
-    FOREIGN KEY (id_merchant) REFERENCES merchants(id)
+    FOREIGN KEY (card) REFERENCES credit_card(card),
+    FOREIGN KEY (id_merchant) REFERENCES merchant(id)
 );
 
+-- Define relationships
+-- merchant_category (1) <-----> (0 to many) merchant
+-- merchant (1) <-----> (0 to many) transaction
+-- credit_card (1) <-----> (0 to many) transaction
+-- card_holder (1) <-----> (0 to many) credit_card
 
--- Define relationships (optional, but helpful for documentation)
--- merchant_categories (1) <-----> (0 to many) merchants
--- merchants (1) <-----> (0 to many) transactions
--- credit_cards (1) <-----> (0 to many) transactions
--- card_holders (1) <-----> (0 to many) credit_cards
